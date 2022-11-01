2588d85049dfb1cc00f675dae444d071bfcc1788
JMURv <architect.lock@outlook.com>
2022-11-02 01:02:10 +0300

    linter fixes

--- page_loader/page.py
+++ page_loader/page.py
@@ -9,7 +9,7 @@ def download(url, output_dir):
     site_name = url2name(url)  # Get the right name for file
     html_outpath = os.path.join(output_dir, f"{site_name}.html")
     html, media_files = prepare_assets(url, output_dir)
-    logging.info(f'Trying to write the file...')
+    logging.info('Trying to write the file...')
     with open(html_outpath, 'w', encoding='UTF-8') as f:
         f.write(html)
     logging.info(f'Successful! File path: {html_outpath}')
